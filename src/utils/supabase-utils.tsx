c64ff04c1287c15c23d09d1dad8bd52d554581e1
Gabriel H S Miiller <46598049+gabriel-hsmiiller@users.noreply.github.com>
2023-02-10 11:48:50 -0300

    Update supabase-utils.tsx

--- src/utils/supabase-utils.tsx
+++ src/utils/supabase-utils.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://jkhzkwcukolwkbnqhalf.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpraHprd2N1a29sd2tibnFoYWxmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NTUzNjcyNSwiZXhwIjoxOTkxMTEyNzI1fQ.y5rTC98oN9D4RxBPTu5ypNBACiyYWHSlOAGVLqHm0-s';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
